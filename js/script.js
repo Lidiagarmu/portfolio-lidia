4a54d9c8d5b389b4bc1f80f502e9e40d5f16b06b
Lidia Garcia <lidiagarmu@gmail.com>
2025-08-17 16:05:12 +0200

    haciendo responsivo el portfolio a diferentes pantallas

--- js/script.js
+++ js/script.js
@@ -2,8 +2,14 @@
 const navLinks = document.querySelectorAll('header nav a ');
 const logoLink = document.querySelector('.logo');
 const sections = document.querySelectorAll('section');
+const  menuIcon= document.querySelector('#menu-icon');
+const navbar = document.querySelector('header nav');
 
 
+menuIcon.addEventListener('click', () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+})
 
 const activePage = () => {
     const header = document.querySelector('header');
@@ -26,6 +32,8 @@ const activePage = () => {
         sections.forEach(section => {
         section.classList.remove('active');
     });
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
 }
 
 
